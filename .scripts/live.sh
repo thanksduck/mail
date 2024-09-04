f9d99cd8f001f8b4f3707d31715a2199e465df86
sivm99 <git.v7gr4@simplelogin.fr>
2024-09-04 17:10:17 +0530

    Deployment Integration

---
+++ .scripts/live.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+echo "Starting To Live the 1 Alias Service Api"
+
+git pull
+## we will check if pull was successful
+if [ $? -eq 0 ]; then
+    echo "Pull was successful"
+else
+    echo "Pull was not successful"
+    exit 1
+fi
+
+if [ -f config.env ]; then
+    echo "File config.env exists."
+else
+    cp $SECRET_PATH/.env/config.env .
+    echo "config.env file copied from $SECRET_PATH/config.env"
+    exit 1
+fi
+
+# we will first check if docker compose is up and running
+if [ $(docker compose ps | grep -c "Up") -eq 0 ]; then
+    echo "Docker compose is not running"
+    docker compose up -d
+else
+    echo "Docker compose is running"
+    docker compose down
+    echo "Docker compose is down"
+    docker compose up -d
+fi
+
+echo "deployment was successful"
