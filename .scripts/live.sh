7874d984e72d831d796243e4aefeb9d0c6be7042
thanksduck <b0thodr8@duck.com>
2024-09-05 13:22:15 +0530

    compose rebuild

--- .scripts/live.sh
+++ .scripts/live.sh
@@ -23,12 +23,12 @@ fi
 # we will first check if docker compose is up and running
 if [ $(docker compose ps | grep -c "Up") -eq 0 ]; then
     echo "Docker compose is not running"
-    docker compose up -d
+    docker compose up -d --build
 else
     echo "Docker compose is running"
     docker compose down
     echo "Docker compose is down"
-    docker compose up -d
+    docker compose up -d --build
 fi
 
 echo "deployment was successful"
